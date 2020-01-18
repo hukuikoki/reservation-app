6708f361aa5a7d784b9a8854adb7823f9dc072b5
kokifukui <koki.fukui@imjp.co.jp>
2020-01-18 23:29:50 +0900

    ヘッダーコンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
